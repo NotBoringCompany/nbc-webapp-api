52b9fac2ae860cfee5521789dacf3a55b21c9e40
suwandre <suwandresukijat@gmail.com>
2023-04-18 10:46:01 +0200

    removed deprecated ioutil import, replaced with os

--- api/kos/kos.go
+++ api/kos/kos.go
@@ -2,7 +2,6 @@ package kos
 
 import (
 	"fmt"
-	"io/ioutil"
 	"os"
 	"strings"
 
@@ -13,7 +12,7 @@ import (
 	"github.com/joho/godotenv"
 )
 
-// loads the Key Of Salvation contract (ETH Blockchain)
+// loads the Key Of Salvation contract (ETH mainnet)
 func loadKOS(caller bind.ContractCaller, transactor bind.ContractTransactor, filterer bind.ContractFilterer) (*bind.BoundContract, error) {
 	err := godotenv.Load()
 	if err != nil {
@@ -28,22 +27,15 @@ func loadKOS(caller bind.ContractCaller, transactor bind.ContractTransactor, fil
 		return nil, err
 	}
 
-	// opens and then reads the contents of the KOS ABI
-	kosAbi, err := os.Open("abi/KeyOfSalvation.json")
+	kosAbiContentBytes, err := os.ReadFile("abi/KeyOfSalvation.json")
 	if err != nil {
 		return nil, err
 	}
-
-	kosAbiContentBytes, err := ioutil.ReadAll(kosAbi)
-	if err != nil {
-		return nil, err
-	}
-
-	defer kosAbi.Close()
 
 	// converts the array of bytes obtained from reading the abi to a string
 	kosAbiContent := string(kosAbiContentBytes)
 
+	// KOS address on ETH mainnet
 	kosAddr := common.HexToAddress("0x34BFF2Dbf20cF39dB042cb68D42D6d06fdbd85D3")
 	abi, err := abi.JSON(strings.NewReader(kosAbiContent))
 	if err != nil {
