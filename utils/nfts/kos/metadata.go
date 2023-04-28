387ee6c484d07b31fdd132bbff05eea453bdab31
suwandre <suwandresukijat@gmail.com>
2023-04-28 19:03:30 +0200

    removed commented out function

--- utils/nfts/kos/metadata.go
+++ utils/nfts/kos/metadata.go
@@ -60,44 +60,6 @@ func FetchMetadata(tokenId int) (*models.KOSMetadata, error) {
 	return &metadata, nil
 }
 
-// func FetchMetadata(tokenId int) *models.KOSMetadata {
-// 	// create a new HTTP client
-// 	client := &http.Client{}
-
-// 	url := os.Getenv("KOS_URI") + fmt.Sprint(tokenId) + ".json"
-// 	req, err := http.NewRequest("GET", url, nil)
-// 	if err != nil {
-// 		log.Fatal("Error while creating request", err)
-// 		return nil
-// 	}
-
-// 	// send the request and get the response
-// 	res, err := client.Do(req)
-// 	if err != nil {
-// 		log.Fatal("Error while sending request", err)
-// 		return nil
-// 	}
-
-// 	defer res.Body.Close()
-
-// 	// read the response body into a byte array
-// 	body, err := io.ReadAll(res.Body)
-// 	if err != nil {
-// 		log.Fatal("Error while reading response body", err)
-// 		return nil
-// 	}
-
-// 	// unmarshal the bytes array into a `KOSMetadata` struct instance.
-// 	var metadata models.KOSMetadata
-// 	err = json.Unmarshal(body, &metadata)
-// 	if err != nil {
-// 		log.Fatal("Error while unmarshalling response body", err)
-// 		return nil
-// 	}
-
-// 	return &metadata
-// }
-
 /*
 `FetchSimplifiedMetadata` returns a more simplified version of a Key Of Salvation's metadata (returns a KOSSimplifiedMetadata struct).
 
