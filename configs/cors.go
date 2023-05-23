941397d076c6c097d529593d14c06ac695223485
suwandre <suwandresukijat@gmail.com>
2023-05-23 11:09:20 +0200

    added dev branch to cors alloworigins

--- configs/cors.go
+++ configs/cors.go
@@ -7,7 +7,7 @@ Returns a cors.Config instance to allow requests from webapp.nbcompany.io
 */
 func CorsConfig() cors.Config {
 	return cors.Config{
-		AllowOrigins: "https://webapp.nbcompany.io,http://localhost:3000",
+		AllowOrigins: "https://webapp.nbcompany.io,http://localhost:3000,https://nbc-webapp-git-dev-not-boring-company.vercel.app",
 		AllowMethods: "GET,POST,PUT,DELETE,OPTIONS",
 		AllowHeaders: "Origin,X-Requested-With,Content-Type,Accept,session-token",
 	}
